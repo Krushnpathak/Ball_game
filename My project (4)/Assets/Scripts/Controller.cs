9419eecf0f8c195763ce144a64c805af729cdabf
Krushnpathak <79698306+Krushnpathak@users.noreply.github.com>
2022-06-08 09:21:21 +0530

    Ball movement and camera follow

---
+++ My project (4)/Assets/Scripts/Controller.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float ms = 10f;
+    private float xin;
+    private float yin;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        inputs();
+    }
+    void FixedUpdate()
+    {
+        move();
+    }
+
+    private void inputs(){
+        xin = Input.GetAxis("Horizontal");
+        yin = Input.GetAxis("Vertical");
+    }
+
+    private void move(){
+        rb.AddForce(new Vector3(xin,0f,yin)*ms);
+    }
+}
---
+++ My project (4)/Assets/Scripts/cameracontroller.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameracontroller : MonoBehaviour
+{
+    public GameObject follow;
+    public float xoff,yoff,zoff;
+    void Update()
+    {
+        transform.position = follow.transform.position + new Vector3(xoff,yoff,zoff);
+        transform.LookAt(follow.transform.position);
+    }
+}
